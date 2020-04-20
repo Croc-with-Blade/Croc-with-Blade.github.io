5f068591cc905cdabc7436988fdd5fdaac222516
unknown <manumanurly@gmail.com>
2020-04-20 21:46:04 +0530

    task 1 hacker mode complete

--- game.js
+++ game.js
@@ -19,81 +19,121 @@ var myArray1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 1
 var layer = shuffle(myArray1);
 var order = 1;
 var running = 0;
-var r = 0.1;
+var r1 = 0.1, r2 = 0.1, r3 = 0.1;
 var runTimer = 0.001;
 var finalTime = 0;
 var bestTimes = [];
+var difficulty = 5;
+var length;
 
+var storedTimes = new Array(3);
+var bestTimes = new Array(3);
 
-var storedTimes = JSON.parse(localStorage.getItem("bestTimes"));
-
-if (storedTimes === null) {
-
-} else {
-  bestTimes = storedTimes;
-  var length = bestTimes.length;
-  document.getElementById("best-time").innerHTML = "<h3>Best Time:</h3>";
-  if (length <= 5) {
-    for (let j = 0; j < length; j++) {
-      document.getElementById("best-time").innerHTML += "<h5>" + bestTimes[j] + " s</h5>";
-    }
-  } else {
-    for (let j = 0; j < 5; j++) {
-      document.getElementById("best-time").innerHTML += "<h5>" + bestTimes[j] + " s</h5>";
-    }
-  }
+for (var i = 0; i < 3; i++) {
+  storedTimes[i] = new Array(5);
+  bestTimes[i] = new Array(5);
 }
 
 
 
 
-document.getElementById("container").addEventListener("click", handleClick);
+storedTimes = JSON.parse(localStorage.getItem("bestTimes"));
 
+
+
+document.getElementById("easy").addEventListener("click", easyClick);
+document.getElementById("moderate").addEventListener("click", moderateClick);
+document.getElementById("hard").addEventListener("click", hardClick);
+
+function easyClick() {
+  this.style.backgroundColor = "#3e8e41";
+  difficulty = 0;
+  document.getElementById("container").addEventListener("click", handleClick);
+  document.getElementById("container").innerHTML = "<h1 id='title'><a href='#'>Click here to Start!</a></h1>";
+
+  document.getElementById("easy").removeEventListener("click", easyClick);
+  document.getElementById("moderate").removeEventListener("click", moderateClick);
+  document.getElementById("hard").removeEventListener("click", hardClick);
+
+  if (storedTimes === null) {
+
+  } else {
+    bestTimes = storedTimes;
+    length = bestTimes[0].length;
+    document.getElementById("best-time").innerHTML = "<h3>Best Time:</h3>";
+    for (let j = 0; j < length; j++) {
+        if(bestTimes[difficulty][j] === null){
+
+        }
+        else {
+          document.getElementById("best-time").innerHTML += "<h5>" + bestTimes[difficulty][j] + " s</h5>";
+        }
+
+
+    }
+
+  }
+
+}
+
+function moderateClick() {
+  this.style.backgroundColor = "#FFA500";
+  difficulty = 1;
+  document.getElementById("container").addEventListener("click", handleClick);
+  document.getElementById("container").innerHTML = "<h1 id='title'><a href='#'>Click here to Start!</a></h1>";
+  document.getElementById("easy").removeEventListener("click", easyClick);
+  document.getElementById("moderate").removeEventListener("click", moderateClick);
+  document.getElementById("hard").removeEventListener("click", hardClick);
+
+  if (storedTimes === null) {
+
+  } else {
+    bestTimes = storedTimes;
+    length = bestTimes[1].length;
+    document.getElementById("best-time").innerHTML = "<h3>Best Time:</h3>";
+    for (let j = 0; j < length; j++) {
+        if(bestTimes[difficulty][j] === null){
+
+        }
+        else {
+          document.getElementById("best-time").innerHTML += "<h5>" + bestTimes[difficulty][j] + " s</h5>";
+        }
+
+
+    }
+  }
+}
+
+function hardClick() {
+  this.style.backgroundColor = "red";
+  difficulty = 2;
+  document.getElementById("container").addEventListener("click", handleClick);
+  document.getElementById("container").innerHTML = "<h1 id='title'><a href='#'>Click here to Start!</a></h1>";
+  document.getElementById("easy").removeEventListener("click", easyClick);
+  document.getElementById("moderate").removeEventListener("click", moderateClick);
+  document.getElementById("hard").removeEventListener("click", hardClick);
+
+  if (storedTimes === null) {
+
+  } else {
+    bestTimes = storedTimes;
+    length = bestTimes[2].length;
+    document.getElementById("best-time").innerHTML = "<h3>Best Time:</h3>";
+    for (let j = 0; j < length; j++) {
+        if(bestTimes[difficulty][j] === null){
+
+        }
+        else {
+          document.getElementById("best-time").innerHTML += "<h5>" + bestTimes[difficulty][j] + " s</h5>";
+        }
+
+
+    }
+  }
+}
 
 
 function handleClick() {
-  const container = document.getElementById("container");
-
-  function endCountdown() {
-    for (let i = 0; i < 20; ++i) {
-      const button = document.createElement("div");
-      container.appendChild(button);
-      button.innerHTML = "<p>" + layer[i] + "</p>";
-      button.id = "block" + i;
-      document.getElementById("block" + i).addEventListener("click", clicked);
-    }
-  }
-
-  function clicked() {
-
-    var value = parseInt(this.textContent);
-    animatePress(this.id);
-
-    if (value <= 20 && value === order) {
-      playSound("click");
-      num = value + 20;
-      this.innerHTML = "<p>" + num + "</p>";
-      this.style.backgroundColor = "rgba(0, 255, 0," + r + ")";
-      this.style.color = "white";
-      order++;
-      r += 0.05;
-    } else if (value < 40 && value > 20 && value === order) {
-      playSound("click");
-      this.style.backgroundColor = "black";
-      this.style.color = "black";
-      order++;
-    } else if (value === 40) {
-      playSound("bell");
-      running = value;
-      for (i = 0; i < 40; ++i) {
-        document.getElementById("block" + i).style.display = "none";
-
-      }
-    }
-
-
-
-  }
 
   var count = 3;
   var countDown = setInterval(function() {
@@ -108,37 +148,32 @@ function handleClick() {
           document.getElementById("Timer").innerHTML = "<h3>Timer:</h3><h4>" + runTimer + "</h4>";
         } else {
           finalTime = runTimer.toFixed(3);
-
-
           storedTimes = JSON.parse(localStorage.getItem("bestTimes"));
-
           if (storedTimes === null) {
-            bestTimes.push(finalTime);
+            bestTimes[difficulty].push(finalTime);
             document.getElementById("best-time").innerHTML = "<h3>Best Time:</h3>";
             localStorage.setItem("bestTimes", JSON.stringify(bestTimes));
           } else {
             bestTimes = storedTimes;
-            bestTimes.push(finalTime);
-            length = bestTimes.length;
-            bestTimes.sort(function(a, b) {
+            bestTimes[difficulty].push(finalTime);
+            length = bestTimes[difficulty].length;
+            bestTimes[difficulty].sort(function(a, b) {
               return a - b
             });
             localStorage.setItem("bestTimes", JSON.stringify(bestTimes));
             document.getElementById("best-time").innerHTML = "<h3>Best Time:</h3>";
-            if (length <= 5) {
-              for (let j = 0; j < length; j++) {
-                document.getElementById("best-time").innerHTML += "<h5>" + bestTimes[j] + " s</h5>";
-              }
-            } else {
-              for (let j = 0; j < 5; j++) {
-                document.getElementById("best-time").innerHTML += "<h5>" + bestTimes[j] + " s</h5>";
-              }
+            for (let j = 0; j < length; j++) {
+                if(bestTimes[difficulty][j] === null){
+
+                }
+                else {
+                  document.getElementById("best-time").innerHTML += "<h5>" + bestTimes[difficulty][j] + " s</h5>";
+                }
+
+
             }
+
           }
-
-
-
-
           document.getElementById("Timer").innerHTML = "<h3>Timer:</h3><h4>" + finalTime + " s</h4>";
           document.getElementById("container").innerHTML = "<h1> Your time: " + finalTime + " s</h1>";
           clearInterval(timer);
@@ -154,14 +189,120 @@ function handleClick() {
   }, 1000);
 
 
+  const container = document.getElementById("container");
+  function endCountdown() {
+    for (let i = 0; i < 20; ++i) {
+      const button = document.createElement("div");
+      container.appendChild(button);
+      button.innerHTML = "<p>" + layer[i] + "</p>";
+      button.id = "block" + i;
+      document.getElementById("block" + i).addEventListener("click", clicked);
+    }
+  }
+
+  function clicked() {
+
+    var value = parseInt(this.textContent);
+    animatePress(this.id);
+    if(difficulty === 0) {
+      if (value <= 20 && value === order) {
+        playSound("click");
+        num = value + 20;
+        this.innerHTML = "<p>" + num + "</p>";
+        this.style.backgroundColor = "rgba(0, 255, 0," + r1 + ")";
+        this.style.color = "white";
+        order++;
+        r1 += 0.05;
+      } else if (value < 40 && value > 20 && value === order) {
+        playSound("click");
+        this.style.backgroundColor = "black";
+        this.style.color = "black";
+        order++;
+      } else if (value === 40) {
+        playSound("bell");
+        running = value;
+        for (i = 0; i < 40; ++i) {
+          document.getElementById("block" + i).style.display = "none";
+
+        }
+      }
+    }
+    else if (difficulty === 1) {
+      if (value <= 20 && value === order) {
+        playSound("click");
+        num = value + 20;
+        this.innerHTML = "<p>" + num + "</p>";
+        this.style.backgroundColor = "rgba(0, 255, 0," + r1 + ")";
+        this.style.color = "white";
+        order++;
+        r1 += 0.05;
+      } else if (value <= 40 && value > 20 && value === order) {
+        playSound("click");
+        num = value + 20;
+        this.innerHTML = "<p>" + num + "</p>";
+        this.style.backgroundColor = "rgba(255,165,0," + r2 + ")";
+        this.style.color = "white";
+        order++;
+        r2 += 0.05;
+      } else if (value < 60 && value > 40 && value === order) {
+        playSound("click");
+        this.style.backgroundColor = "black";
+        this.style.color = "black";
+        order++;
+      }else if (value === 60) {
+        playSound("bell");
+        running = value;
+        for (i = 0; i < 20; ++i) {
+          document.getElementById("block" + i).style.display = "none";
+
+        }
+      }
+    }
+    else if (difficulty === 2
+    ) {
+      if (value <= 20 && value === order) {
+        playSound("click");
+        num = value + 20;
+        this.innerHTML = "<p>" + num + "</p>";
+        this.style.backgroundColor = "rgba(0, 255, 0," + r1 + ")";
+        this.style.color = "white";
+        order++;
+        r1 += 0.05;
+      } else if (value <= 40 && value > 20 && value === order) {
+        playSound("click");
+        num = value + 20;
+        this.innerHTML = "<p>" + num + "</p>";
+        this.style.backgroundColor = "rgba(255,165,0," + r2 + ")";
+        this.style.color = "white";
+        order++;
+        r2 += 0.05;
+      } else if (value <= 60 && value > 40 && value === order) {
+        playSound("click");
+        num = value + 20;
+        this.innerHTML = "<p>" + num + "</p>";
+        this.style.backgroundColor = "rgba(255,0,0," + r3 + ")";
+        this.style.color = "white";
+        order++;
+        r3 += 0.05;
+      }else if (value < 80 && value > 60 && value === order) {
+        playSound("click");
+        this.style.backgroundColor = "black";
+        this.style.color = "black";
+        order++;
+      }else if (value === 80) {
+        playSound("bell");
+        running = value;
+        for (i = 0; i < 20; ++i) {
+          document.getElementById("block" + i).style.display = "none";
+
+        }
+      }
+    }
+
+
+  }
+
 }
-
-document.getElementById("new-game").addEventListener("click", function(){
-  playSound("newgame");
-});
-
-
-
 function animatePress(currentColor) {
   document.getElementById(currentColor).classList.add("pressed");
   setTimeout(function () {
